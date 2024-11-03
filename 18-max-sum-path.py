2012409f26a415e733011f983753d87f91587d0a
Harris-Go <harrisgoodman1@gmail.com>
2024-11-03 16:29:52 +0000

    Completed Project 18

--- 18-max-sum-path.py
+++ 18-max-sum-path.py
@@ -36,49 +36,20 @@ row = []
 
 pyramid = [[75], [95, 64], [17, 47, 82], [18, 35, 87, 10], [20, 4, 82, 47, 65], [19, 1, 23, 75, 3, 34], [88, 2, 77, 73, 7, 63, 67], [99, 65, 4, 28, 6, 16, 70, 92], [41, 41, 26, 56, 83, 40, 80, 70, 33], [41, 48, 72, 33, 47, 32, 37, 16, 94, 29], [53, 71, 44, 65, 25, 43, 91, 52, 97, 51, 14], [70, 11, 33, 28, 77, 73, 17, 78, 39, 68, 17, 57], [91, 71, 52, 38, 17, 14, 91, 43, 58, 50, 27, 29, 48], [63, 66, 4, 68, 89, 53, 67, 30, 73, 16, 69, 87, 40, 31], [4, 62, 98, 27, 23, 9, 70, 98, 73, 93, 38, 53, 60, 4, 23]]
 
-largest = []
-for k in range(15):
-    largest.append([])
-    max_num = 0
-    loc_max = 0
-    for j in range(len(pyramid[k])):
-        if pyramid[k][j] >= max_num:
-            max_num = pyramid[k][j]
-            loc_max = j
-    largest[k].append(max_num)
-    largest[k].append(loc_max)
+def check_highest_combinations(list1, list2):
+    for index1 in range(len(list1)):
+        combo1 = list1[index1] + list2[index1]
+        combo2 = list1[index1] + list2[index1 + 1]
+        if combo1 > combo2:
+            list1[index1] = combo1
+        else:
+            list1[index1] = combo2
+    return list1
 
-print(largest)
+def largest_combo(pyramid_list):
+    for pyramid_level in range(len(pyramid_list) - 1):
+        current_level = len(pyramid_list) - (pyramid_level + 2)
+        pyramid_list[current_level] = check_highest_combinations(pyramid_list[current_level], pyramid_list[current_level + 1])
+    print(pyramid_list[0])
 
-def max_below_fn(y,x):
-    if y == 14:
-        return [0, 0, 7]
-    if pyramid[y+1][x] > pyramid[y+1][x+1]:
-        max_below = [pyramid[y+1][x], y+1, x]
-    else:
-        max_below = [pyramid[y+1][x+1], y+1, x+1]
-    return max_below
-
-def max_above_fn(y,x):
-    if y == 0:
-        return [0, 0, 0]
-    if y == x:
-        return [pyramid[y-1][x-1], y-1, x-1] 
-    elif x == 0:
-        return [pyramid[y-1][x], y-1, x]
-    elif pyramid[y-1][x] > pyramid[y-1][x-1]:
-        return [pyramid[y-1][x], y-1, x]
-    else:
-        return [pyramid[y-1][x], y-1, x-1]
-
-largest_str = []
-for p in range(len(largest)):
-    below = max_below_fn(p,largest[p][1])
-    above = max_above_fn(p,largest[p][1])   
-    largest_str.append([])
-    largest_str[p].append(largest[p][0] + below[0] + above[0])
-    largest_str[p].append(above[2])
-    largest_str[p].append(below[2])
-    print("Below:",below)
-    print("Above:",above)
-    print(largest_str)
+largest_combo(pyramid)
