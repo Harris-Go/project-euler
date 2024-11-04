0315bdf72cce2ebc659488d469d62a3b416dad82
Harris-Go <harrisgoodman1@gmail.com>
2024-11-04 19:20:19 +0000

    Made code more efficient

--- 21-30/21-amicable-numbers.py
+++ 21-30/21-amicable-numbers.py
@@ -8,12 +8,9 @@ def sum_of_divisors(number):
     return divisor_total
 
 total_sum = 0
-already_found = []
 for numbers in range(1,10000):
     divisor_sum = sum_of_divisors(numbers)
     divisor_sum2 = sum_of_divisors(divisor_sum)
-    if divisor_sum2 == numbers and divisor_sum != divisor_sum2 and numbers not in already_found:
-        already_found.append(divisor_sum)
-        print(numbers, divisor_sum, divisor_sum2)
-        total_sum += (divisor_sum + divisor_sum2)
+    if divisor_sum2 == numbers and divisor_sum != divisor_sum2:
+        total_sum += (numbers)
 print(total_sum)
